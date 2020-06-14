3062a89c6246fd1728acc6a2606f3172f8a6ab07
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-06-15 04:28:31 +0530

    Removed unnecassery pauses

--- install.sh
+++ install.sh
@@ -7,16 +7,12 @@ echo "################################"
 echo
 echo "g++ -c colors.cpp -o colors.o"
 `g++ -c server/colors.cpp -o server/colors.o`
-sleep 1
 echo "g++ -c inputs.cpp -o inputs.o"
 `g++ -c server/inputs.cpp -o server/inputs.o`
-sleep 1
 echo "g++ -c interface.cpp -o interface.o"
 `g++ -c server/interface.cpp -o server/interface.o`
-sleep 1
 echo "g++ -c server.cpp -o server.o"
 `g++ -c server/server.cpp -o server/server.o`
-sleep 1
 echo "g++ -pthread -o main main.cpp *.o"
 `g++ -pthread -o sillyrat server/main.cpp server/*.o`
 `rm server/*.o`
